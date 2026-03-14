25c74740e148214e6394a8cfe97bfd6c48f5a243
APuskunigis <arnaspusk204@gmail.com>
2026-03-14 13:08:56 +0000

    added bullets which fire in a default direction where you click the mouse

--- Bullet.h
+++ Bullet.h
@@ -1,13 +1,13 @@
 #include <SFML/Graphics.hpp>
 
-class Player {
+class Bullet {
 public:
-    Player(sf::Texture& texture);
-    void handleInput(float deltaTime);
+    Bullet(float x, float y, sf::Texture& texture);
+    void flyForwards(float deltaTime);
     void update(float deltaTime);
     void draw(sf::RenderWindow& window);
 
 private:
     sf::Sprite sprite;
-    float speed = 150.0f;
+    float speed = 100.0f;
 };
