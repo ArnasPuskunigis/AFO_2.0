67528aea91fb5bffe1511faf87dae2c4eac430fc
APuskunigis <arnaspusk204@gmail.com>
2026-03-14 09:36:45 +0000

    added a clock for delta time, and keypress input

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
-int main() {
+int main()
+{
     sf::RenderWindow window(sf::VideoMode(1000, 700), "AFO 2.0");
-    window.setFramerateLimit(60);
 
-    while (window.isOpen()) {
+    //Start clock for deltatime (Makes it so speed is not FPS dependent)
+    sf::Clock clock;
+
+    while (window.isOpen())
+    {
+        float deltaTime = clock.restart().asSeconds();
         sf::Event event;
+
         while (window.pollEvent(event))
+        {
+
             if (event.type == sf::Event::Closed)
+            {
                 window.close();
+                std::cout << "Game was closed!" << std::endl;
+            }
+
+            if (event.type == sf::Event::KeyPressed)
+                std::cout << event.key.code << " was pressed!" << std::endl;
+        }
 
         window.clear(sf::Color::Black);
         window.display();
