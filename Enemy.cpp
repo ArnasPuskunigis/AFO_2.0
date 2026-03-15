bb2c76d4e13fa27f05121bc0434e85acc1635cc3
APuskunigis <arnaspusk204@gmail.com>
2026-03-15 11:12:44 +0000

    enemy now detects when a bullet collides with it

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,15 +1,35 @@
 #include "Enemy.h"
+#include "Bullet.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
 
-Enemy::Enemy(float x, float y, sf::Texture& texture) {
+Enemy::Enemy(float x, float y, sf::Texture &texture)
+{
     sprite.setTexture(texture);
     sprite.setScale(0.2f, 0.2f);
     sprite.setPosition(x, y);
 }
 
-void Enemy::update(float deltaTime) {
+void Enemy::checkForBulletCollisions(std::vector<Bullet>& bullets)
+{
+    sf::FloatRect bounds = sprite.getGlobalBounds();
 
+    for (Bullet &Bullet : bullets)
+    {
+        if (bounds.intersects(Bullet.getSprite().getGlobalBounds()) && Bullet.getCanCollide() == true)
+        {
+            Bullet.disableCollision();
+            std::cout << "A bullet has hit this enemy!" << std::endl;
+        }
+    }
 }
 
-void Enemy::draw(sf::RenderWindow& window) {
+void Enemy::update(float deltaTime, std::vector<Bullet>& bullets)
+{
+    checkForBulletCollisions(bullets);
+}
+
+void Enemy::draw(sf::RenderWindow &window)
+{
     window.draw(sprite);
 }
